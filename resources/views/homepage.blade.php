26c14bedc22d2b9e2dea06cee573b20d221fd146
lcver <crackover1092@gmail.com>
2022-02-15 12:17:18 +0700

    [modify] Finishing all elements

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,151 @@
     {{-- Jumbotron --}}
     <div class="jumbotron jumbotron-fluid">
         <div class="banner-image">
-            {{-- <img src="{{asset('storage/assets/images/banner/bg-01.jpeg')}}" alt="background banner"> --}}
+            <img src="{{asset('storage/assets/images/banner/bg-01.jpeg')}}" alt="background banner">
         </div>
     </div>
+    <div class="layout-1">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p>The Product</p>
+                        <h5>Premium Coffee Beans</h5>
+                        <p>Our selected premium coffee beans, freshly picked from the volcanic soils.</p>
+                    </div>
+                    <img src="{{asset('storage/assets/images/thumb/coffee-candy.png')}}" width="500" alt="...">
+                    <div class="clearfix"></div>
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p>The Product</p>
+                        <h5>Second slide label</h5>
+                        <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                    <img src="{{asset('storage/assets/images/thumb/coffee-candy.png')}}" width="500" alt="...">
+                    <div class="clearfix"></div>
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p>The Product</p>
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                    <img src="{{asset('storage/assets/images/thumb/coffee-candy.png')}}" width="500" alt="...">
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="layout-2">
+        <div class="container">
+            <div class="card col-md-12">
+                <div class="card-image">
+                    <img class="card-img-top rounded" src="{{asset('storage/assets/images/thumb/the-process.png')}}" alt="Card image">
+                </div>
+                <div class="card-body caption-thumbnail">
+                    <p class="card-text">The Process</p>
+                    <h5 class="card-title">Innovation born from Volcanic Mountain in Indonesia</h5>
+                    <a href="#" class="btn btn-outline-light">Find Out more about the excellence</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="layout-3 pt-4 pb-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="card-img-top rounded" src="{{asset('storage/assets/images/thumb/the-journey.png')}}" alt="Card image">
+                        </div>
+                        <div class="card-body caption-thumbnail">
+                            <p class="card-text">The Process</p>
+                            <h5 class="card-title">Innovation born from Volcanic Mountain in Indonesia</h5>
+                            <a href="#" class="btn btn-outline-light">Find Out more about the excellence</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="card-img-top rounded" src="{{asset('storage/assets/images/thumb/the-philosophy.png')}}" alt="Card image">
+                        </div>
+                        <div class="card-body caption-thumbnail">
+                            <p class="card-text">The Process</p>
+                            <h5 class="card-title">Innovation born from Volcanic Mountain in Indonesia</h5>
+                            <a href="#" class="btn btn-outline-light">Find Out more about the excellence</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-1">
+            <div class="container">
+                <p><i class="fas fa-globe"></i> Select Country</p>
+            </div>
+        </div>
+        <div class="footer-2">
+            <div class="container">
+                <div class="row">
+                    <div class="section-1 col-md-2">
+                        <div class="footer-menu-list">
+                            <p>OUR COLLECTION</p>
+                            <ul>
+                                <li>Coffee Candy</li>
+                                <li>Ready to Drink</li>
+                                <li>Ready to Served</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="section-2 col-md-2">
+                        <div class="footer-menu-list">
+                            <p>OUR STORY</p>
+                            <ul>
+                                <li>The Process</li>
+                                <li>The Journey</li>
+                                <li>The Philosophy</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="section-3 col-md-2">
+                        <div class="footer-menu-list">
+                            <p>STAY UPDATED</p>
+                            <ul>
+                                <li>News & Events</li>
+                                <li>Contact us</li>
+                                <li>Find your local representative</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="section-4 col-md-6">
+                        <p>SUBSCRIBE TO THE LATEST NEWS OF KOPIKO</p>
+                        <form action="">
+                            <input type="email" name="email" placeholder="Your Email Address">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-3">
+            <div class="container">
+                <img src="{{asset('storage/assets/images/coffee-house-logo.png')}}" alt="Coffee house logo">
+                <div class="section-1">
+                    <p>&copy;2021 The Coffe House</p>
+                    <ul class="footer-sosmed">
+                        <li><i class="fab fa-facebook"></i></li>
+                        <li><i class="fab fa-instagram"></i></li>
+                        <li><i class="fab fa-youtube"></i></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,9 @@
     {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     {{-- Bootstrap Icon --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"> --}}
+    {{-- Fontawesome Icon --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- Style --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script>
@@ -24,25 +26,35 @@
 </head>
 <body>
   <header>
-    <a href="#" class="logo">
-        <img src="{{asset('storage/assets/images/coffee-house-logo.png')}}" alt="Coffee house logo">
-    </a>
-    <div class="toggle" onclick="toggleMenu()">
-      <i class="bi bi-justify"></i>
-    </div>
-    <ul class="menu" >
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Our Product</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <ul class="menu-footer">
-            <li><a href=""><i class="bi bi-instagram"></i></a></li>
-            <li><a href=""><i class="bi bi-facebook"></i></a></li>
-            <li><a href=""><i class="bi bi-youtube"></i></a></li>
+    <div class="container">
+      <div class="justify-content">
+        <a href="#" class="logo">
+            <img src="{{asset('storage/assets/images/coffee-house-logo.png')}}" alt="Coffee house logo">
+        </a>
+        <div class="toggle" onclick="toggleMenu()">
+          {{-- <i class="bi bi-justify"></i> --}}
+          <i class="fas fa-bars"></i>
+        </div>
+        <ul class="menu" >
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Our Product</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <ul class="menu-footer">
+              {{-- <li><a href=""><i class="bi bi-instagram"></i></a></li>
+              <li><a href=""><i class="bi bi-facebook"></i></a></li>
+              <li><a href=""><i class="bi bi-youtube"></i></a></li> --}}
+              <li><a href=""><i class="fab fa-instagram"></i></a></li>
+              <li><a href=""><i class="fab fa-facebook"></i></a></li>
+              <li><a href=""><i class="fab fa-youtube"></i></a></li>
+            </ul>
         </ul>
-    </ul>
-</header>
+      </div>
+    </div>
+  </header>
   @yield('content')
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
 </body>
 </html>
